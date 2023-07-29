f8876a8d683731626eecde2237ac2ef62b09fafd
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-07-29 09:14:09 -0400

    feat: add treesitter plugin

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", },
+    cmd = "TSUpdateSync",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = {
+                "c",
+                "cpp",
+                "cuda",
+                "lua",
+                "fortran",
+                "cmake",
+                "make",
+                "bash",
+                "python",
+            },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
+}
