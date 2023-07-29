10b794b35fd97d17556a9257094677b60a501bf6
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-07-29 18:01:02 -0400

    feat: edit gitsigns symbols

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -10,6 +10,15 @@ return {
     },
     {
         "lewis6991/gitsigns.nvim",
-        opts = {},
+        opts = {
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "▎" },
+                topdelete = { text = "契" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+        },
     },
 }
