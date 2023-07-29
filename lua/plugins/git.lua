758dee837ebb16cb3f2710bc58a07eb02097d2b5
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-07-29 15:57:31 -0400

    feat: add git plugins

---
+++ lua/plugins/git.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "kdheepak/lazygit.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        keys = {
+            {"<leader>gg", "<cmd>LazyGit<cr>", desc = "Toggle lazygit"},
+        },
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {},
+    },
+}
