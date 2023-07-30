08fc591863124a039d3734c0c804393a93c234f0
Pranav Sivaraman <14294205+pranav-sivaraman@users.noreply.github.com>
2023-07-29 22:28:17 -0400

    refactor: remove comment key macro

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -2,12 +2,4 @@
 return {
     "numToStr/Comment.nvim",
     opts = {},
-    keys = {
-        { "<leader>/",
-            function()
-                require("Comment.api").toggle.linewise.count(vim.v.count > 0 and vim.v.count or 1)
-            end,
-            desc = "Toggle comment"
-        },
-    },
 }
